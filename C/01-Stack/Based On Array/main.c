23a6bf467d9333d7010ed533cc5a707de621cba5
moaz khaled <mo3az.khalid.2003@gmail.com>
2024-10-07 01:45:18 +0300

    Update main.c

--- C/01-Stack/Based On Array/main.c
+++ C/01-Stack/Based On Array/main.c
@@ -12,11 +12,13 @@ int main(void)
     scanf("%d" , &size);
     for(int i = 0 ; i < size ;i++)
         push(&S1 , i * 9);
-
     copy_stack(S1 , &S2);
     pop(&S2);
     pop(&S2);
-    printf("size of Stack2 after pop Elements is : %d\n" , size_of_stack(S2));
+    printf("size of Stack2 after pop Two Elements is : %d\n" , size_of_stack(S2));
     print_stack(S2);
+    clear_stack(&S2);
+    printf("The size of Stack2 After Clear it is : %d\n" , size_of_stack(S2));
+    printf("%d",Top(S2));
     return 0;
 }
